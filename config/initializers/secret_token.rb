cc299125c344b40a15344f263d0c57e7469ae050
moggsterb <moggster@moggster.com>
2012-05-11 09:08:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordage::Application.config.secret_token = '593fc6b2f2cf9a79df6fd44d0edb43ef06532d78a4010824555679cbfbfbd42c71de5141940072ca92c0fe43a0a9f5f19a7608042a25bb7a1eeb6d8c6154ae1f'
